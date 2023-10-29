55d5f1f62519dc4ee742435e620368651ab0634d
yarogono <70641418+yarogono@users.noreply.github.com>
2023-10-29 20:02:44 +0900

    Client/NetworkManager

---
+++ Client/Assets/Scripts/Managers/NetworkManager.cs
@@ -0,0 +1,51 @@
+using Google.Protobuf;
+using ServerCore;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEditor.Sprites;
+using UnityEngine;
+
+public class NetworkManager : CustomSingleton<NetworkManager>
+{
+
+    ServerSession _session = new ServerSession();
+
+    void Awake()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        // DNS (Domain Name System)
+        string host = Dns.GetHostName();
+        IPHostEntry ipHost = Dns.GetHostEntry(host);
+        IPAddress ipAddr = ipHost.AddressList[0];
+        IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+        Connector connector = new Connector();
+
+        connector.Connect(endPoint,
+            () => { return _session; },
+        1);
+    }
+
+    void Update()
+    {
+        List<PacketMessage> list = PacketQueue.Instance.PopAll();
+        foreach (PacketMessage packet in list)
+        {
+            Action<PacketSession, IMessage> handler = PacketManager.Instance.GetPacketHandler(packet.Id);
+            if (handler != null)
+                handler.Invoke(_session, packet.Message);
+        }
+    }
+
+    public void Send(ArraySegment<byte> sendBuff)
+    {
+        _session.Send(sendBuff);
+    }
+
+}
