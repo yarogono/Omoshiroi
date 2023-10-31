c8b425da49935fd51ea1d5f3ef02eeeb80c432c7
jyo217 <solbaram217@gmail.com>
2023-10-31 21:14:11 +0900

    장비 장착/탈착 구조 설계 완료 #28

--- Client/Assets/Scripts/Character/HealthSystem.cs
+++ Client/Assets/Scripts/Character/HealthSystem.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class HealthSystem : MonoBehaviour
 {
-    public CharacterStats stats;
- 
+    private CharacterStats stats;
+
+    HealthSystem(CharacterStats characterStats)
+    {
+        stats = characterStats;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +21,18 @@ public class HealthSystem : MonoBehaviour
     void Update()
     {
         
+    }
+
+    /// <summary>
+    /// 체력 회복 요소를 추가한다면 그냥 - 데미지를 넣어서 구현하면 될 듯 함.
+    /// </summary>
+    public void TakeDamege(int damege)
+    {
+        stats.BaseHP -= damege;
+        
+        if(stats.BaseHP <= 0)
+        {
+            //플레이어 사망 또는 무언가를 처리하는 부분
+        }
     }
 }
