758600665486168444ce2f0c99b788c4b4a56a1e
jyo217 <solbaram217@gmail.com>
2023-11-01 20:07:06 +0900

    CharcterStats, CharacterBaseStats 멤버변수 추가 #28

--- Client/Assets/Scripts/Character/CharacterBaseStats.cs
+++ Client/Assets/Scripts/Character/CharacterBaseStats.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class CharacterBaseStats : ScriptableObject
 {
     [Header("Character's base stat")]
-    [SerializeField] private int baseHP;
-    [SerializeField] private int baseDEF;
-    [SerializeField] private float baseAttackSpeed;
-    [SerializeField] private int baseAttackPower;
-    [SerializeField] private int baseCriticalRate;
-    [SerializeField] private float baseCriticalPower;
+    [SerializeField] [Tooltip("기본 체력")] private int baseHP;
+    [SerializeField] [Tooltip("기본 방어력")] private int baseDEF;
+    [SerializeField] [Tooltip("기본 공격 속도")] private float baseAttackSpeed;
+    [SerializeField] [Tooltip("기본 공격력")] private int baseAttackPower;
+    [SerializeField] [Tooltip("기본 크리티컬 확률")] private int baseCriticalRate;
+    [SerializeField] [Tooltip("기본 크리티컬 시 피해량 배율")] private float baseCriticalPower;
+    [SerializeField] [Tooltip("기본 이동 속도")] private float baseMoveSpeed;
+    [SerializeField] [Tooltip("달리기 시 기본 이동속도 배율")] private float baseRunMultiplier;
 
     public int BaseHP { get; }
     public int BaseDEF { get; }
@@ -18,4 +20,6 @@ public class CharacterBaseStats : ScriptableObject
     public int BaseAttackPower { get; }
     public int BaseCriticalRate { get; }
     public float BaseCriticalPower { get; }
+    public float BaseMoveSpeed { get; }
+    public float BaseRunMultiplier { get; }
 }
