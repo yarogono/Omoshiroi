3819df8dfa12925eda4c86e50bd93146b742acc0
jyo217 <solbaram217@gmail.com>
2023-11-01 16:16:30 +0900

    itemIcon, objectPrefab 프로퍼티 추가 #14

--- Client/Assets/Scripts/Items/BaseItem.cs
+++ Client/Assets/Scripts/Items/BaseItem.cs
@@ -13,6 +13,8 @@ public class BaseItem : ScriptableObject
     [SerializeField] private string description;
     [SerializeField] private eItemType itemType;
 
+    public Sprite ItemIcon { get { return itemIcon; } }
+    public GameObject ObjectPrefab { get { return objectPrefab; } }
     public int ItemID { get { return itemID; } }
     public string ItemName { get { return itemName; } }
     public string Description { get { return description; } }
