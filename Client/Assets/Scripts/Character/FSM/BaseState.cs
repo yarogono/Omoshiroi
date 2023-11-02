dff62c610b2748a5a2761a690f130eae1f02f0b2
Hwan007 <ghksdlco@gmail.com>
2023-11-02 12:26:39 +0900

    Animation Controller을 FSM으로 변경

---
+++ Client/Assets/Scripts/Character/FSM/BaseState.cs
@@ -0,0 +1,17 @@
+public class BaseState : IState
+{
+    public void Enter()
+    {
+        
+    }
+
+    public void Execute()
+    {
+        
+    }
+
+    public void Exit()
+    {
+        
+    }
+}
