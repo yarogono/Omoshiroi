a6490eeb5e511c5b4f11388969a2b5f1fb5ebccc
DoDokang318 <110431311+DoDokang318@users.noreply.github.com>
2023-11-03 12:45:18 +0900

    feat: 로딩씬 스타트씬 구현
    ㅇㅇ

---
+++ Client/Assets/Scripts/Utils/LodingFuc.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class LodingFuc : MonoBehaviour
+{
+    static string nextScene;
+
+    [SerializeField]
+    Image ProgressBar;
+
+    public static void LoadScene(string sceneName)
+    {
+        nextScene = sceneName;
+        SceneManager.LoadScene("LoadingScene");
+    }
+
+    private void Start()
+    {
+        StartCoroutine(LoadSceneProcess());
+    }
+
+    IEnumerator LoadSceneProcess()
+    {
+        AsyncOperation op = SceneManager.LoadSceneAsync(nextScene);
+        op.allowSceneActivation = false;
+
+        float timer = 0f;
+        while (!op.isDone)
+        {
+            yield return null;
+            if (op.progress < 0.9f)
+            {
+                ProgressBar.fillAmount = op.progress;
+            }
+            else
+            {
+                timer += Time.unscaledDeltaTime * 0.5F;
+                ProgressBar.fillAmount = Mathf.Lerp(0.7f, 1f, timer);
+                if (ProgressBar.fillAmount >= 1f)
+                {
+                    op.allowSceneActivation = true;
+                    yield break;
+                }
+            }
+        }
+    }
+}
