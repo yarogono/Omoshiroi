503e94c452572ada56f57557a537f2e10a15e2fc
Hwan007 <ghksdlco@gmail.com>
2023-11-06 23:24:11 +0900

    Sync Move 추가.

--- Client/Assets/Scripts/Character/FSM/CharacterGroudState.cs
+++ Client/Assets/Scripts/Character/FSM/CharacterGroudState.cs
@@ -36,5 +36,6 @@ public class CharacterGroundState : BaseState
     protected override void MoveEvent(Vector2 direction)
     {
         MoveCharacter(direction);
+        _stateMachine.Character.Sync.SendC_MovePacket();
     }
 }
