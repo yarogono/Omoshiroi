23bcaa1f5a08a00b4a87fbc450187c0d48a2f3e6
Hwan007 <ghksdlco@gmail.com>
2023-11-07 10:53:34 +0900

    Clone Sync를 위한 움직임 스크립트 기초 완성.

---
+++ Client/Assets/Scripts/Character/Clone/CloneMovement.cs
@@ -0,0 +1,88 @@
+using UnityEngine;
+
+public class CloneMovement : MonoBehaviour
+{
+    // 외부에서 객체의 정보를 가져가기 위함
+    public Vector3 Velocity { get => currentVelocity; }
+    public Vector3 Position { get => transform.position; }
+
+    // 호출 시점 관련
+    public float AfterLastOrderTime { get => Time.realtimeSinceStartup - _lastOrderTime; }
+    public float LastOrderTime {  get => _lastOrderTime; }
+    private float _lastOrderTime;
+
+    // 속도 관련
+    private bool _velFlag;
+    private Vector3 LastVelocity;
+    private Vector3 currentVelocity;
+
+    // 위치 관련
+    private bool _posFlag;
+    private Vector3 _lastPosition;
+
+    // Sync 모듈
+    private CloneSync _sync;
+
+    // 조금씩 느려지도록
+    [Header("오랫동안 업데이트가 없으면 조금씩 느려져 멈추도록 설정함.")]
+    [SerializeField][Tooltip("멈추는 시간")] private float _dampTime;
+    private Vector3 _dampingVelocity;
+    
+
+    void Start()
+    {
+        gameObject.TryGetComponent<CloneSync>(out _sync);
+        // TODO
+        // _sync의 이벤트에 MoveClone 혹은 Move를 등록하도록 한다.
+    }
+
+    void Update()
+    {
+        UpdateMovement();
+        if (AfterLastOrderTime > 60)
+            SmoothlyStop();
+    }
+
+    public void MoveClone(Vector3 position, Vector3 velocity)
+    {
+        _lastPosition = position;
+        _posFlag = true;
+        currentVelocity = velocity;
+        _velFlag = true;
+        _lastOrderTime = Time.realtimeSinceStartup;
+    }
+
+    public void MoveClone(Vector3 position)
+    {
+        _lastPosition = position;
+        _posFlag = true;
+        _lastOrderTime = Time.realtimeSinceStartup;
+    }
+
+    private void UpdateMovement()
+    {
+        Vector3 deltaPosition = transform.position;
+
+        if (_posFlag)
+        {
+            deltaPosition = _lastPosition;
+            _posFlag = false;
+        }
+
+        if (_velFlag)
+        {
+            deltaPosition += currentVelocity * Time.deltaTime;
+            LastVelocity = currentVelocity;
+            _velFlag = false;
+        }
+        else
+            deltaPosition += LastVelocity * Time.deltaTime;
+
+        transform.position = deltaPosition;
+    }
+
+    private void SmoothlyStop()
+    {
+        LastVelocity = Vector3.SmoothDamp(LastVelocity, Vector3.zero, ref _dampingVelocity, _dampTime);
+    }
+}
