28bd909fd260d4a657ff139ef6bb934e49a1e05e
jyo217 <solbaram217@gmail.com>
2023-11-08 18:04:53 +0900

    HealthSystem 개선 및 서버 통신 부분 틀 구현 #118

--- Client/Assets/Scripts/Character/HealthSystem.cs
+++ Client/Assets/Scripts/Character/HealthSystem.cs
@@ -7,6 +7,8 @@ public class HealthSystem : MonoBehaviour
 
     [SerializeField] private CharacterStats stats;
 
+    PlayerHPReq req = new PlayerHPReq();
+    PlayerHPRes res = new PlayerHPRes();
 
     // Start is called before the first frame update
     void Start()
@@ -24,13 +26,12 @@ public class HealthSystem : MonoBehaviour
     /// 초과 치유 또는 사망 조건에 대한 예외처리 포함. 치유의 경우 -데미지를 입력값으로 받도록 처리하면 됨. 
     /// </summary>
     /// <param name="damege"></param>
-    public void TakeDamege(int damege)
+    public void ChangeHP(int changeAmount)
     {
-        int remain = stats.Hp - damege;
+        int remain = stats.Hp + changeAmount;
 
         if(remain <= 0)
         {
-            stats.SetHP(0);
             //플레이어 사망 또는 무언가를 처리하는 부분
         }
         else if (stats.MaxHp < remain)
@@ -39,8 +40,39 @@ public class HealthSystem : MonoBehaviour
         }
         else
         {
-            stats.SetHP(remain);
         }
 
+        req.MaxHp = stats.MaxHp;
+        req.CurrentHP = stats.Hp;
+        req.HPChangeAmount = changeAmount;
+
+        //서버에 자신의 ID 와 입은 피해량 정보를 전달하는 내용
+        SendHPReq();
+
+        stats.SetHP(remain);
     }
+
+    /// <summary>
+    /// 플레이어 체력 정보를 보낼 url 경로와 PlayerHPRes 타입에 담겨야 할 멤버변수를 정해야 한다.
+    /// </summary>
+    private void SendHPReq()
+    {
+        WebManager.Instance.SendPostRequest<PlayerHPRes>("url 경로", req, SetHPRes);    
+    }
+
+    private void SetHPRes(PlayerHPRes newRes){
+        res = newRes;
+    }
+}
+
+public class PlayerHPReq
+{
+    public int MaxHp { get; set; }
+    public int CurrentHP { get; set; }
+    public int HPChangeAmount { get; set; }
+}
+
+public class PlayerHPRes
+{
+    
 }
