c532a006100b5509d25b1f154051900fb626681e
Hwan007 <ghksdlco@gmail.com>
2023-11-08 19:47:33 +0900

    BaseAttack 업데이트

--- Client/Assets/Scripts/Character/Attack/BaseAttack.cs
+++ Client/Assets/Scripts/Character/Attack/BaseAttack.cs
@@ -4,5 +4,46 @@ using UnityEngine.Events;
 
 public class BaseAttack : MonoBehaviour
 {
-    
+    protected string _makerTag;
+    public int Damage { get; private set; }
+
+    /// <summary>
+    /// 플래이어가 요청할 때 사용
+    /// </summary>
+    /// <param name="dataContainer"></param>
+    /// <param name="tag"></param>
+    public virtual void Initalize(CharacterDataContainer dataContainer, string tag)
+    {
+        _makerTag = tag;
+        // Damage = ....
+    }
+
+    /// <summary>
+    /// NPC, Clone이 요청할 때 사용
+    /// </summary>
+    /// <param name="dataContainer"></param>
+    /// <param name="tag"></param>
+    public virtual void Initalize(CloneDataContainer dataContainer, string tag)
+    {
+        _makerTag = tag;
+        // Damage = ....
+    }
+
+    /// <summary>
+    /// NPC에게 부딪혔을 때 사용
+    /// </summary>
+    /// <param name="dataContainer"></param>
+    public virtual void ApplyDamage(CloneDataContainer dataContainer)
+    {
+        
+    }
+
+    /// <summary>
+    /// 플래이어에게 부딪혔을 때 사용
+    /// </summary>
+    /// <param name="dataContainer"></param>
+    public virtual void ApplyDamage(CharacterDataContainer dataContainer)
+    {
+
+    }
 }
--- Client/Assets/Scripts/Character/Attack/MeleeAttack.cs
+++ Client/Assets/Scripts/Character/Attack/MeleeAttack.cs
@@ -4,5 +4,43 @@ using UnityEngine;
 
 public class MeleeAttack : BaseAttack
 {
-    
+    // 플래이어가 요청할 때 사용
+    public override void Initalize(CharacterDataContainer dataContainer, string tag)
+    {
+        base.Initalize(dataContainer, tag);
+        // 추가적으로 해야되는 작업
+    }
+
+    // NPC, Clone이 요청할 때 사용
+    public override void Initalize(CloneDataContainer dataContainer, string tag)
+    {
+        base.Initalize(dataContainer, tag);
+        // 추가적으로 해야되는 작업
+    }
+
+    // NPC에게 부딪혔을 때 사용
+    public override void ApplyDamage(CloneDataContainer dataContainer)
+    {
+        base.ApplyDamage(dataContainer);
+        // 추가적으로 해야되는 작업
+    }
+
+    // 플래이어에게 부딪혔을 때 사용
+    public override void ApplyDamage(CharacterDataContainer dataContainer)
+    {
+        base.ApplyDamage(dataContainer);
+        // 추가적으로 해야되는 작업
+    }
+
+    // 플래이어, 벽, 물체
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag(_makerTag))
+        {
+            ApplyDamage(other.GetComponent<CharacterDataContainer>());
+        }
+    }
+
+    // TODO
+    // NPC는 어떻게 감지해야되는지 고민
 }
