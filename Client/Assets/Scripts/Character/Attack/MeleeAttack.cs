23efffe1f92033b373faa0d9ddb66cdf3d6b467b
Hwan007 <ghksdlco@gmail.com>
2023-11-08 18:06:38 +0900

    공격 제작 시작.

--- Client/Assets/Scripts/Character/Attack/MeleeAttack.cs
+++ Client/Assets/Scripts/Character/Attack/MeleeAttack.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MeleeAttack : MonoBehaviour
+public class MeleeAttack : BaseAttack
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
