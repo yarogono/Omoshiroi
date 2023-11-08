340076787d0daa6c1398f7be625ada486cef5682
DoDokang318 <110431311+DoDokang318@users.noreply.github.com>
2023-11-08 16:15:17 +0900

    feat:Rest API 아이템 데이터 Response 테스트
    서버에서받아온 아이템 정보 인벤토리에 들어가는것 확인

--- Client/Assets/Scripts/Contoller/SceneController.cs
+++ Client/Assets/Scripts/Contoller/SceneController.cs
@@ -1,24 +1,29 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
+using System.Collections.Generic;
 
 public class SceneController : MonoBehaviour
 {
     [SerializeField]
-    private string sceneName = "PlayerTestScene";
+    private string sceneName = "InventoryBackup";
     [SerializeField]
     private Button BtnChangeScene;
+
+    public static List<PlayerItemRes> items = new List<PlayerItemRes>();
     private void Start()
     {
         BtnChangeScene.onClick.AddListener(() =>
         {          
             //login테스트
-            AccountLoginReq req = new AccountLoginReq() { AccountPassword = "qwer" };
+            AccountLoginReq req = new AccountLoginReq() { AccountPassword = "12345",AccountName = "12345" };
             AccountLoginRes newRes = null;
             WebManager.Instance.SendPostRequest<AccountLoginRes>("account/login", req, res =>
-            {                
-                newRes = res;        
+            {           
+                
+                newRes = res;
+                items= newRes.Items;
+                Debug.Log( newRes);
             });
             LoadingScenController.LoadScene(sceneName);
         });
