b28b1f3e698cb5de3b510783f8b7067ee6941539
Hwan007 <ghksdlco@gmail.com>
2023-11-09 20:21:23 +0900

    근접 공격 완성

--- Client/Assets/Scripts/Character/Attack/MeleeAttack.cs
+++ Client/Assets/Scripts/Character/Attack/MeleeAttack.cs
@@ -10,14 +10,19 @@ public class MeleeAttack : BaseAttack
         base.Initalize(dataContainer, tag);
         // 추가적으로 해야되는 작업
         var Magic = dataContainer.Equipments.GetEquippedItem(eItemType.Magic) as BaseMagic;
-        Damage = dataContainer.Stats.AtkPower;
     }
 
     public override void ApplyDamage(HealthSystem health)
     {
-        base.ApplyDamage(health);
+        Damage = _data.Stats.AtkPower;
+        // 구체적인 대미지 계산을 맡기기
+        // Stats에 대미지 계산 메소드를 만든다.
+        // Damage = dataContainer.Stats.GetDamage(health);
+        // base.ApplyDamage(health);
         health.TakeDamage(Damage);
+        // TakeDamage에서 방어력에 따라서 입는 대미지를 줄인다.
         gameObject.SetActive(false);
+        // TODO
         // 피격음 재생
     }
 
@@ -29,11 +34,13 @@ public class MeleeAttack : BaseAttack
     {
         if (!other.CompareTag(_makerTag))
         {
-            var data = other.GetComponent<DataContainer>();
-            ApplyDamage(data.Health);
+            if (other.gameObject.layer == (other.gameObject.layer & AttackManager.Instance.TargetLayer))
+            {
+                var data = other.GetComponent<DataContainer>();
+                ApplyDamage(data.Health);
+            }
+            // TODO
+            // NPC는 어떻게 감지해야되는지 고민
         }
     }
-
-    // TODO
-    // NPC는 어떻게 감지해야되는지 고민
 }
