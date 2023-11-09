3189549395ab9322c88d382f8017785afa7ac8de
DoDokang318 <110431311+DoDokang318@users.noreply.github.com>
2023-11-09 17:34:33 +0900

    테스트
    ㅇㅇ

--- Client/Assets/Scripts/Character/Attack/RangeAttack.cs
+++ Client/Assets/Scripts/Character/Attack/RangeAttack.cs
@@ -26,6 +26,8 @@ public class RangeAttack : BaseAttack
 
     public void Launch()
     {
+
+
         // 공격생명주기
         Invoke(nameof(Deactivate), lifeTime);
 
@@ -44,7 +46,7 @@ public class RangeAttack : BaseAttack
         // 이곳에 충돌한 대상에 대한 처리 로직을 구현
         Debug.Log(other.name);
         
-        if (other.tag == "Clone")
+        if (other.tag == "Pilot")
         {
 
             var characterData = other.GetComponent<CharacterDataContainer>();
