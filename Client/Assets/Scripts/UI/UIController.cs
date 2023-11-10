d9d9b865b591753585fd92b7fa8cd30cbf3c5bde
DoDokang318 <110431311+DoDokang318@users.noreply.github.com>
2023-11-10 19:59:39 +0900

    UI:Robby씬
    Robby씬

--- Client/Assets/Scripts/UI/UIController.cs
+++ Client/Assets/Scripts/UI/UIController.cs
@@ -3,23 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class UIController: MonoBehaviour
 {
     public Slider HpBar;
 
     [SerializeField] private float MaxHp;
     [SerializeField] private float CurHp;
+    LeaveGame leaveGame;
+    public  Button BtnLeavGame;
 
     private void Start()
     {
         HpBar.value = CurHp / MaxHp;
+        leaveGame = GetComponent<LeaveGame>();
 
+        BtnLeavGame.onClick.AddListener(() =>
+        {
+            if (leaveGame)
+            {
+                leaveGame.LeaveGameRoom();
+            }
+            else
+            {
+                Debug.LogError("Component null");
+            }
+        });
     }
 
-    private void Update()
-    {
-        
-    }
+        private void Update()
+        {
+         
+        }
 
     private void HandlerHp()
     {
