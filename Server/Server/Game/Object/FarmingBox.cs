d55fcb99dbc21c3a6281cd105d621ade520e5591
yarogono <70641418+yarogono@users.noreply.github.com>
2023-11-10 20:47:34 +0900

    파밍박스 로직 초기코드 추가

---
+++ Server/Server/Game/Object/FarmingBox.cs
@@ -0,0 +1,17 @@
+﻿using Google.Protobuf.Protocol;
+using Org.BouncyCastle.Asn1.Mozilla;
+
+namespace Server.Game.Object
+{
+    public class FarmingBox : GameObject
+    {
+
+        public FarmingBox()
+        {
+            ObjectType = GameObjectType.FarmingBox;
+        }
+
+        public FarmingBoxInfo BoxInfo { get; set; } = new FarmingBoxInfo();
+
+    }
+}
