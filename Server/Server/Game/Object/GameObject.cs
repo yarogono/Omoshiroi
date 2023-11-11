0fe002751d845f90cfe42709ffbe6d58c2cc8994
yarogono <70641418+yarogono@users.noreply.github.com>
2023-11-11 12:49:29 +0900

    파밍박스 초기로직

--- Server/Server/Game/Object/GameObject.cs
+++ Server/Server/Game/Object/GameObject.cs
@@ -15,56 +15,5 @@ namespace Server.Game.Object
         public GameRoom Room { get; set; }
 
         public ObjectInfo Info { get; set; } = new ObjectInfo();
-        public StatInfo Stat { get; private set; } = new StatInfo();
-
-        public GameObject()
-        {
-            Info.StatInfo = Stat;
-        }
-
-        public virtual void HpDamage(C_HpDamage hpDamagePacket)
-        {
-            if (Room == null)
-                return;
-
-            int maxHp = hpDamagePacket.MaxHp;
-            int currentHp = hpDamagePacket.CurrentHp;
-            int objectId = hpDamagePacket.ObjectId;
-            int changeAmount = hpDamagePacket.ChangeAmount;
-
-            int damagedHp = Math.Max(Stat.Hp -= changeAmount, 0);
-
-            if (damagedHp != currentHp)
-                return;
-
-            Stat.Hp = currentHp;
-            Info.StatInfo.MaxHp = maxHp;
-
-            S_HpDamage resHpDamagePacket = new S_HpDamage();
-            resHpDamagePacket.ObjectId = objectId;
-            resHpDamagePacket.CurrentHp = Stat.Hp;
-
-            Room.Broadcast(resHpDamagePacket, objectId);
-        }
-
-        public virtual void OnDead(GameObject attacker)
-        {
-            if (Room == null)
-                return;
-
-            S_Die diePacket = new S_Die();
-            diePacket.ObjectId = Id;
-            diePacket.AttackerId = attacker.Id;
-            Room.Broadcast(diePacket);
-
-            Room.LeaveGame(Id);
-
-            Stat.Hp = Stat.MaxHp;
-            Info.PosInfo.PosX = 0;
-            Info.PosInfo.PosY = 0;
-            Info.PosInfo.PosZ = 0;
-
-            Room.EnterGame(this);
-        }
     }
 }
