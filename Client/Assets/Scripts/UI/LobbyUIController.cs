df27943f3c36b893896b76019a7fa2ac536fc9d1
Hwan007 <ghksdlco@gmail.com>
2023-11-15 12:12:18 +0900

    옵션 창 UI 완료 및 로비 씬 제작중

--- Client/Assets/Scripts/UI/LobbyUIController.cs
+++ Client/Assets/Scripts/UI/LobbyUIController.cs
@@ -5,31 +5,14 @@ using UnityEngine.UI;
 
 public class LobbyUIController : MonoBehaviour
 {
-    [SerializeField] private Button BtnReadyToBattle;
-    [SerializeField] private Button BtnEnterBattle;
-    [SerializeField] private GameObject UIReadyToBattle;
-    [SerializeField] private string sceneName;
 
     private void Awake()
     {
-        init();
+
     }
     void Start()
     {
-        BtnReadyToBattle.onClick.AddListener(() =>
-        {
-            UIReadyToBattle.SetActive(true);
-        });
-        BtnEnterBattle.onClick.AddListener(() =>
-        {
-            SoundManager.Instance.Clear();
-            LoadingScenController.LoadScene(sceneName);
-        });
 
-    }
-    void init()
-    {
-        UIReadyToBattle.SetActive(false);    
     }
 
     public void OpenOption()
