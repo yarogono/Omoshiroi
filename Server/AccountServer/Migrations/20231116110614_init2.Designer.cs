b6a0af5b4c8fdacd97ea22c31f584d294cfd94ac
yarogono <70641418+yarogono@users.noreply.github.com>
2023-11-16 20:10:28 +0900

    AccountServer/DBdesign

--- Server/AccountServer/Migrations/20231116110614_init2.Designer.cs
+++ Server/AccountServer/Migrations/20231116110614_init2.Designer.cs
@@ -2,6 +2,7 @@
 using AccountServer.DB;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AccountServer.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231116110614_init2")]
+    partial class init2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
