f541cbbb298a33ec2c350fa51122f0f01c29907e
jyo217 <solbaram217@gmail.com>
2023-11-17 10:24:56 +0900

    적 NPC AI 배회 상태 임시 구현 #214

--- Client/Assets/Scripts/Character/NPC/NPCAIController.cs
+++ Client/Assets/Scripts/Character/NPC/NPCAIController.cs
@@ -6,35 +6,57 @@ using UnityEngine.UI;
 
 public class NPCAIController : MonoBehaviour
 {
-    //목적지 좌표
-    [SerializeField] public List<Vector3> targets;
     
     //길찾기용 컴포넌트
-    [SerializeField] public NavMeshAgent agent;
+    [SerializeField] private NavMeshAgent agent;
 
-    [SerializeField] public float minWanderDistance = 5f; 
-    [SerializeField] public float maxWanderDistance = 15f;
-    [SerializeField] public float detectDistance = 10f;
+    //목적지 좌표
+    [SerializeField] private List<Vector3> destinations;
 
-    private Vector3 destination;
+    [SerializeField] private float characterRadiusMultiplier = 1.5f;
+    [SerializeField] private float minWanderDistance = 5f; 
+    [SerializeField] private float maxWanderDistance = 15f;
+    [SerializeField] private float detectDistance = 10f;
+
+    private Vector2 viewDirection = new Vector2();
+    private Vector2 aimDirection = new Vector2();
+    private Vector3 curDestination;
+    private float characterRadius;
+
+    public List<Vector3> Destinations { get { return destinations; } private set { destinations = value; } }
+    public NavMeshAgent Agent { get { return agent; } private set { agent = value; } }
+    public float MinWanderDistance { get { return minWanderDistance; } private set { minWanderDistance = value; } }
+    public float MaxWanderDistance { get { return maxWanderDistance; } private set { maxWanderDistance = value; } }
+    public float DetectDistance { get { return detectDistance; } private set { detectDistance = value; } }
+
+    public Vector2 ViewDirection { get { return viewDirection; } private set { viewDirection = value; } }
+    public Vector2 AimDirection { get { return aimDirection; } private set { aimDirection = value; } }
+
+
+    private void Awake()
+    {
+        characterRadius = agent.radius * characterRadiusMultiplier;
+    }
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        int index = Random.Range(0, targets.Count);
-        destination = targets[index];
-        //목적지 설정.
-        agent.destination = destination;
+        curDestination = GetNextDestination();
+        agent.destination = curDestination;
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        //현재 위치에서 hit 까지의 거리가 매우 근접했다면 새 목적지를 설정한다.
-        if (Vector3.Distance(transform.position, agent.destination) < 1.5f)
+        //현재 목적지에 매우 근접했다면 새 목적지를 설정한다.
+        if (Vector3.Distance(transform.position, curDestination) < characterRadius)
         {
-            destination = GetWanderLocation();
-            agent.destination = destination;
+            if(destinations.Count <= 0)
+            {
+                curDestination = GetWanderLocation();
+                agent.destination = curDestination;
+                return;
+            }
         }
     }
 
@@ -47,5 +69,12 @@ public class NPCAIController : MonoBehaviour
         NavMesh.SamplePosition(transform.position + (Random.onUnitSphere * Random.Range(minWanderDistance, maxWanderDistance)), out hit, maxWanderDistance, NavMesh.AllAreas);
         Debug.Log($"목적지 설정 : ({hit.position})");
         return hit.position;
+    }
+
+    //목적지 리스트에서 랜덤한 다음 목적지 좌표를 반환한다.
+    Vector3 GetNextDestination()
+    {
+        int index = Random.Range(0, destinations.Count);
+        return destinations[index];
     }
 }
